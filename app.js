268c3606cc595498fae6cfa5f50ee772ef9bc103
Kaiyu <anne550323@gmail.com>
2021-06-14 17:35:48 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
