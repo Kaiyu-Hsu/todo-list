1d83806f59d3b8a70c5ce52fe8cab3436b50d913
Kaiyu <anne550323@gmail.com>
2021-06-17 16:51:28 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000
+const PORT = process.env.PORT || 3000
 
 // 把樣板引擎指定為 Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -22,6 +24,6 @@ app.use(methodOverride('_method'))
 // 將 request 導入路由器
 app.use(routes)
 
-app.listen(port, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
